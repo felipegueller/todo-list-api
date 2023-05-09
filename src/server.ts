7245e630852823f7ce145c4cc30146be7e57c458
Felipe Gueller <felipe.gueller@gmail.com>
2023-05-09 18:17:11 -0300

    :sparkles: feat: setting the dependeces to api connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
 import 'dotenv'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app = express()
 const serverPort = process.env.API_PORT
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(serverPort, () => console.log(`The server is running on port ${serverPort}.`))

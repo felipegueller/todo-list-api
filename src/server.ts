c40ee1228dd33114239d3fe2408682eefe0ff13a
Felipe Gueller <felipe.gueller@gmail.com>
2023-05-09 21:20:03 -0300

    :sparkles: feat: adding the list main route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express'
-import 'dotenv'
+import 'dotenv/config'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+
+import { listRouter } from './routes/list.routes'
 
 const app = express()
 const serverPort = process.env.API_PORT
 
 app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/lists', listRouter)
 
 app.listen(serverPort, () => console.log(`The server is running on port ${serverPort}.`))
